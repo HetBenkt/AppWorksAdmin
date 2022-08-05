71457b954b93b3cc978d157351df775abdf317f9
Antal Bos <hetbenkt@gmail.com>
2022-08-05 12:44:54 +0200

    mockito-junit-jupiter dependency

--- SoapLayer/src/test/java/nl/bos/mock/AuthenticationMockTest.java
+++ SoapLayer/src/test/java/nl/bos/mock/AuthenticationMockTest.java
@@ -1,7 +1,10 @@
 package nl.bos.mock;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+@ExtendWith(MockitoExtension.class)
 class AuthenticationMockTest {
 
     @Test
