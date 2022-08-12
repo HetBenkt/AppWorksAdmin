a187772cf27eff09a03f0217e6f3504d3326a9b5
Antal Bos <hetbenkt@gmail.com>
2022-08-12 22:37:34 +0200

    Read property file from static block with try with resources

--- SoapLayer/src/main/java/nl/bos/awp/AppWorksPlatformImpl.java
+++ SoapLayer/src/main/java/nl/bos/awp/AppWorksPlatformImpl.java
@@ -1,11 +1,23 @@
 package nl.bos.awp;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 public enum AppWorksPlatformImpl implements AppWorksPlatform {
     INSTANCE;
+    private static final Properties properties = new Properties();
+
+    static {
+        try (InputStream inStream = AppWorksPlatformImpl.class.getClassLoader().getResourceAsStream("config.properties")) {
+            properties.load(inStream);
+        } catch (IOException e) {
+            throw new RuntimeException("Error reading config.properties");
+        }
+    }
 
     public boolean ping() {
-        String healthCheckURL = "http://192.168.56.110:8080/home/system/app/mp/health/ready";
-        AppWorksPlatformService awpService = new AppWorksPlatformServiceImpl(healthCheckURL);
+        AppWorksPlatformService awpService = new AppWorksPlatformServiceImpl(properties.getProperty("health_url"));
         return awpService.ping();
     }
 }
