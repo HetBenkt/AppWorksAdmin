6c872df99598739c19b9285b7742085b40b0d3f0
Antal Bos <hetbenkt@gmail.com>
2022-10-17 23:24:25 +0200

    SearchLDAP to get list of methods

--- SoapLayer/src/test/java/nl/bos/integration/TestIntegrationData.java
+++ SoapLayer/src/test/java/nl/bos/integration/TestIntegrationData.java
@@ -12,4 +12,20 @@ public enum TestIntegrationData {
                 </SOAP:Body>
             </SOAP:Envelope>
             """;
+
+    final String requestSearchLdap = """
+            <SOAP:Envelope xmlns:SOAP="http://schemas.xmlsoap.org/soap/envelope/">
+              <SOAP:Body>
+                <SearchLDAP xmlns="http://schemas.cordys.com/1.0/ldap">
+                  <dn>cn=cordys,cn=defaultInst,o=22.3.com</dn>
+                  <scope>2</scope>
+                  <filter>&amp;(objectclass=busmethod)(cn=GetUserDetails*)</filter>
+                  <sort>ascending</sort>
+                  <sortBy />
+                  <returnValues>false</returnValues>
+                  <return />
+                </SearchLDAP>
+              </SOAP:Body>
+            </SOAP:Envelope>
+            """;
 }
